b9817d580f73e33cf55be6f8a9aff578f672cacf
shivamkarn <shivamkarn767@gmail.com>
2025-12-12 10:41:08 +0545

    Added auth db env vairable

--- services/auth-service/src/config/env.ts
+++ services/auth-service/src/config/env.ts
@@ -1,18 +1,17 @@
-import "dotenv/config";
+import 'dotenv/config';
 
-import { createEnv, z } from "@chatapp/common";
+import { createEnv, z } from '@chatapp/common';
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   AUTH_SERVICE_PORT: z.coerce.number().int().min(0).max(65_535).default(6000),
+  AUTH_DB_URL: z.string().url(),
 });
 
 type EnvType = z.infer<typeof envSchema>;
 
 export const env: EnvType = createEnv(envSchema, {
-  serviceName: "auth-service",
+  serviceName: 'auth-service',
 });
 
 export type Env = typeof env;
