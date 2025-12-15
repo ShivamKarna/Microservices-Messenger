ab64757d962b95309d4df8e7ecdc5cd7b484487e
shivamkarn <shivamkarn767@gmail.com>
2025-12-15 21:43:27 +0545

    Completed Register

--- services/auth-service/src/controllers/auth.controller.ts
+++ services/auth-service/src/controllers/auth.controller.ts
@@ -1,3 +1,12 @@
+import { RegisterInput } from '@/types/auth';
+import { asyncHandler } from '@chatapp/common';
 import { RequestHandler } from 'express';
+import { register } from '@/services/auth.service';
 
-export const registerHandler: RequestHandler = async (req, res, next) => {};
+const registerHandler: RequestHandler = asyncHandler(async (req, res, next) => {
+  const payload = req.body as RegisterInput;
+  const tokens = await register(payload);
+  res.status(201).json(tokens);
+});
+
+export { registerHandler };
