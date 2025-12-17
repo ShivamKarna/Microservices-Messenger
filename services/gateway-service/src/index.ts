553fe42a566845baa0952b0c3b5de10b5f784dbb
shivamkarn <shivamkarn767@gmail.com>
2025-12-17 20:56:59 +0545

    Setup index.ts

---
+++ services/gateway-service/src/index.ts
@@ -0,0 +1,35 @@
+import { createApp } from '@/app';
+import { createServer } from 'http';
+import { env } from '@/config/env';
+import { logger } from '@/utils/logger';
+
+const main = async () => {
+  try {
+    const app = createApp();
+    const server = createServer(app);
+    const port = env.GATEWAY_SERVICE_PORT || 6000;
+
+    server.listen(port, () => {
+      logger.info({ port }, 'Gateway Service is running Great');
+    });
+
+    const shutDown = () => {
+      logger.info('Shutting Down the gateway Service...');
+
+      Promise.all([])
+        .catch((e: unknown) => {
+          logger.error({ e }, 'Error During shutdown tasks');
+        })
+        .finally(() => {
+          server.close(() => process.exit(0));
+        });
+    };
+    process.on('SIGINT', shutDown);
+    process.on('SIGTERM', shutDown);
+  } catch (error) {
+    logger.error({ error }, 'Failed to start Auth Service');
+    process.exit(1);
+  }
+};
+
+void main();
