1965f35b918bfbf8eb916f3af92af763e6f43181
shivamkarn <shivamkarn767@gmail.com>
2025-12-18 22:26:07 +0545

    Added body nesting.

--- services/auth-service/src/routes/auth.schema.ts
+++ services/auth-service/src/routes/auth.schema.ts
@@ -1,17 +1,25 @@
 import { z } from '@chatapp/common';
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(7).max(255),
+  body: z.object({
+    email: z.string().email(),
+    password: z.string().min(7).max(255),
+  }),
 });
 export const registerSchema = loginSchema.extend({
-  displayName: z.string().min(3).max(30),
+  body: z.object({
+    displayName: z.string().min(3).max(30),
+  }),
 });
 
 export const refreshSchema = z.object({
-  refreshSchema: z.string(),
+  body: z.object({
+    refreshSchema: z.string(),
+  }),
 });
 
 export const revokeSchema = z.object({
-  userId: z.string().uuid(),
+  body: z.object({
+    userId: z.string().uuid(),
+  }),
 });
